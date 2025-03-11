dc353215c153f92194dcb8b2dfd31fc50021ef4c
oxamyt <dentez.danil@gmail.com>
2025-03-11 18:03:01 +0200

    feat(postRouter): add imageUrl for post model

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -8,15 +8,9 @@ import {
 import passport from "passport";
 import { validateData } from "../middlewares/validationMiddleware";
 import { updateUserDataSchema } from "../validation/schemas";
-import multer from "multer";
+import upload from "../utils/multerSetup";
 
 const userRouter = Router();
-
-const storage = multer.memoryStorage();
-const upload = multer({
-  storage,
-  limits: { fileSize: 1024 * 1024 * 5 },
-});
 
 userRouter.get("/", passport.authenticate("jwt", { session: false }), getUsers);
 
---
+++ src/utils/multerSetup.ts
@@ -0,0 +1,9 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+});
+
+export default upload;
